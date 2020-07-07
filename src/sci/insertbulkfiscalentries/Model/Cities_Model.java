f90cc2cc1566ea4499c179dd775e6835d77982b5
Luan Grabher <lujetix@hotmail.com>
2020-07-07 09:55:16 -0300

    Added method to get map list of cities of database

--- src/sci/insertbulkfiscalentries/Model/Cities_Model.java
+++ src/sci/insertbulkfiscalentries/Model/Cities_Model.java
@@ -1,5 +1,46 @@
 package sci.insertbulkfiscalentries.Model;
 
+import fileManager.FileManager;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import sci.insertbulkfiscalentries.Model.Entities.City;
+import sql.Database;
+
 public class Cities_Model {
+    private Map<String, City> cities = new HashMap<>();
     
+    public Map<String, City> getStatesFromDb(){
+        //clear map
+        cities.clear();
+        
+        //Get Sql script
+        File sqlFile = FileManager.getFile("sql/getStateCities.sql");
+        Map<String, String> sqlChanges =  new HashMap<>();
+        
+        //Get list of database states
+        ArrayList<String[]> databaseCities = Database.getDatabase().select(sqlFile);
+        
+        //Percorre todos estados do banco
+        for (String[] databaseCity : databaseCities) {
+            //Cria objeto
+            City city = new City();
+            city.setCode(Integer.valueOf(databaseCity[0]));
+            city.setName(databaseCity[2]);
+            
+            //Adiciona objeto no mapa
+            cities.put(city.getName(), city);
+        }
+        
+        return cities;
+    }
+
+    public Map<String, City> getCities() {
+        return cities;
+    }
+
+    public void setCities(Map<String, City> cities) {
+        this.cities = cities;
+    }        
 }
