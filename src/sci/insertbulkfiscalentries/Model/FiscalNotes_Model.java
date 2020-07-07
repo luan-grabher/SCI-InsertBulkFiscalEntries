5f3d53df78577004cce9bbb426ca1cf6d9f6fe95
Luan Grabher <lujetix@hotmail.com>
2020-07-07 08:42:06 -0300

    Added cnpj. Added some fields in fiscal notes model.

--- src/sci/insertbulkfiscalentries/Model/FiscalNotes_Model.java
+++ src/sci/insertbulkfiscalentries/Model/FiscalNotes_Model.java
@@ -1,5 +1,17 @@
 package sci.insertbulkfiscalentries.Model;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+import sci.insertbulkfiscalentries.Model.Entities.City;
+import sci.insertbulkfiscalentries.Model.Entities.Cnpj;
+import sci.insertbulkfiscalentries.Model.Entities.Service;
+
 public class FiscalNotes_Model {
+    private String code; //Número de NF
+    private Calendar date;
+    private City city;
+    private Service service;
+    private BigDecimal value;
+    private Cnpj cnpj; 
     
 }
