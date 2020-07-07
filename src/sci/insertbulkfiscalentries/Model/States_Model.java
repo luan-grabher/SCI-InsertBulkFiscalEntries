ccf3037a164fa5406ba6fe656e4e75a43d9057dc
Luan Grabher <lujetix@hotmail.com>
2020-07-07 09:46:35 -0300

    Added method to get map list of states of database

--- src/sci/insertbulkfiscalentries/Model/States_Model.java
+++ src/sci/insertbulkfiscalentries/Model/States_Model.java
@@ -2,9 +2,12 @@ package sci.insertbulkfiscalentries.Model;
 
 import fileManager.FileManager;
 import java.io.File;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import sci.insertbulkfiscalentries.Model.Entities.State;
+import sql.Database;
 
 public class States_Model {
     private Map<String, State> states = new HashMap<>();
@@ -15,9 +18,31 @@ public class States_Model {
         
         //Get Sql script
         File sqlFile = FileManager.getFile("sql/getStates.sql");        
-        String sqlScript = FileManager.getText(sqlFile);
         
+        //Get list of database states
+        ArrayList<String[]> databaseStates = Database.getDatabase().select(sqlFile);
+        
+        //Percorre todos estados do banco
+        for (String[] databaseState : databaseStates) {
+            //Cria objeto
+            State state = new State();
+            state.setCode(Integer.valueOf(databaseState[0]));
+            state.setAbbreviation(databaseState[1]);
+            state.setName(databaseState[2]);
+            state.setAliquot(new BigDecimal(databaseState[3]));
+            
+            //Adiciona objeto no mapa
+            states.put(state.getName(), state);
+        }
         
         return states;
     }
+
+    public Map<String, State> getStates() {
+        return states;
+    }
+
+    public void setStates(Map<String, State> states) {
+        this.states = states;
+    }        
 }
